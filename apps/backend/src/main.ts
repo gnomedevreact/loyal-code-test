8739ab74262dfa2a079bea5cb71349f347213ca1
Miron Brinkovskii <143091438+gnomedevreact@users.noreply.github.com>
2025-09-02 19:40:52 +0200

    Update main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,7 @@ import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
